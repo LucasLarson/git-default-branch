007a68ea6e09d2c806fdadb60dbf99eda6652200
Lucas Larson <LucasLarson@riseup.net>
2021-04-15 18:47:31 -0400

    ✨  initial commit

---
+++ git-default-branch.plugin.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+#
+#     git default-branch
+#     🌳  What’s in a name? It doesn’t matter.
+#
+#     author: Lucas Larson
+#     URL: https://github.com/LucasLarson/git-default-branch
+#     license: GNU Affero General Public License v3.0 or later
+#
+PATH=${PATH}:$(dirname "${0}")/bin
+export PATH
+
+alias gmb=git-default-branch
+alias gdb=git-default-branch
