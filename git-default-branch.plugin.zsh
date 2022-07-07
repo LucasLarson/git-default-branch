69ce4fb46dd029b9df10e40f76bd6e6ffaf84658
Lucas Larson <LucasLarson@riseup.net>
2022-07-07 16:23:56 -0400

    prevent initial `:` in `$PATH`
    `$PATH`’s entries are separated by colons `:`, but `$PATH` should have
    no initial or terminal `:`. This construction prevents the initial `:`
    in all cases.
    
    Signed-off-by: Lucas Larson <LucasLarson@riseup.net>

--- git-default-branch.plugin.zsh
+++ git-default-branch.plugin.zsh
@@ -6,8 +6,10 @@
 #     author: Lucas Larson
 #     URL: https://github.com/LucasLarson/git-default-branch
 #     license: GNU Affero General Public License v3.0 or later
-#
-PATH="${PATH}"':'"$(command dirname -- "${0}")"'/bin'
+
+# append `bin/` to PATH
+# without a leading colon if PATH is empty
+PATH="${PATH:+${PATH-}:}$(command dirname -- "$0")"'/bin'
 export PATH
 
 alias 'gmb'='git-default-branch'
